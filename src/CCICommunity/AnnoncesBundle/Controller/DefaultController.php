4d1dad87fe489e61cccb75cc8a18f1e538c5e0e4
Tiger76 <ayrole.l@gmail.com>
2014-06-20 17:02:15 +0200

    route + controle + twig
    A finir

--- src/CCICommunity/AnnoncesBundle/Controller/DefaultController.php
+++ src/CCICommunity/AnnoncesBundle/Controller/DefaultController.php
@@ -6,8 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function evenementAction()
     {
-        return $this->render('CCICommunityAnnoncesBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('CCICommunityAnnoncesBundle:Default:evenement_list.html.twig', array());
     }
+    
+    /* A revoir
+    public function evenementAction(id)
+    {
+        return $this->render('CCICommunityAnnoncesBundle:Default:evenement_detail.html.twig', array(id));
+    }*/
 }
--- src/CCICommunity/ConseilBundle/Controller/DefaultController.php
+++ src/CCICommunity/ConseilBundle/Controller/DefaultController.php
@@ -6,8 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function adressesAction()
     {
-        return $this->render('CCICommunityConseilBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('CCICommunityConseilBundle:Default:adresses.html.twig', array());
+    }
+    
+    public function liensutilesAction()
+    {
+        return $this->render('CCICommunityConseilBundle:Default:liensutiles.html.twig', array());
     }
 }
